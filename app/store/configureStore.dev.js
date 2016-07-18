41b6f4fdfff109fe161800545503d8c7c9465563
jpsierens <jpsierens1988@gmail.com>
2016-07-18 08:50:11 -0600

    remove explicit call to HMR in configureStore dev

--- app/store/configureStore.dev.js
+++ app/store/configureStore.dev.js
@@ -9,13 +9,5 @@ export default function configureStore(initialState) {
         DevTools.instrument()
     );
 
-    if (module.hot) {
-        // Enable Webpack hot module replacement for reducers
-        module.hot.accept('../reducers', () => {
-            const nextRootReducer = require('../reducers').default;
-            store.replaceReducer(nextRootReducer);
-        });
-    }
-
     return store;
 }
