9ba82b9cd1abe969982eda63563da7aa15c0f83b
Jean-Pierre Sierens <jpsierens1988@gmail.com>
2016-07-17 20:28:40 -0600

    remove thunk from prod. store config

--- app/store/configureStore.prod.js
+++ app/store/configureStore.prod.js
@@ -1,11 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
 export default function configureStore(initialState) {
     return createStore(
         rootReducer,
-        initialState,
-        applyMiddleware(thunk)
+        initialState
     );
 }
