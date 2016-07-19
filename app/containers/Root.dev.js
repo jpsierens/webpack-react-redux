e0127509c550013cf1706f703f25b8230d4b5ce7
jpsierens <jpsierens1988@gmail.com>
2016-07-19 08:44:17 -0600

    removing unnecessary imports in Root.dev.js

--- app/containers/Root.dev.js
+++ app/containers/Root.dev.js
@@ -1,9 +1,7 @@
 import React, { Component, PropTypes } from 'react';
-import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import DevTools from './DevTools';
 import { Router } from 'react-router';
-import { Route, IndexRoute } from 'react-router';
 import routes from '../routes';
 
 export default class Root extends Component {
