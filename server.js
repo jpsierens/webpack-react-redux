0ae3b0b46b95b92ac455d584399e47d687e4dc71
Jean-Pierre Sierens <jpsierens@Jean-Pierres.local>
2016-07-27 09:10:58 -0600

    reducing console logging to minimum

--- server.js
+++ server.js
@@ -6,8 +6,20 @@ new WebpackDevServer(webpack(config), {
     publicPath: config.output.publicPath,
     hot: true,
     historyApiFallback: true,
+    // It suppress error shown in console, so it has to be set to false.
+    quiet: false,
+    // It suppress everything except error, so it has to be set to false as well
+    // to see success build.
+    noInfo: false,
     stats: {
-        colors: true
+      // Config for minimal console.log mess.
+      assets: false,
+      colors: true,
+      version: false,
+      hash: false,
+      timings: false,
+      chunks: false,
+      chunkModules: false
     }
 }).listen(3000, 'localhost', function (err) {
     if (err) {
