fb731b64748499f5b5d34e141b182a22c65ed1e0
jpsierens <jpsierens1988@gmail.com>
2017-01-04 10:23:17 -0600

    about

--- app/components/About.js
+++ app/components/About.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 const About = () =>
     <div>
-        Just a dummy page to showcase react-router!
+        Hey! Thanks for using this example. If you like it, consider starring the repo :))
+        <div>
+            <iframe
+                src="https://ghbtns.com/github-btn.html?user=jpsierens&repo=webpack-react-redux&type=star&size=large"
+                frameBorder="0"
+                allowTransparency="true"
+                scrolling="0">
+            </iframe>
+        </div>
     </div>;
 
 
