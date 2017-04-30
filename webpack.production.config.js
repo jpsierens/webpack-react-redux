27d11b1e1b63edf436a676867b5e455fd2c7ef22
chriddyp <chris@plot.ly>
2017-04-29 22:10:33 -0700

    tyop

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -7,7 +7,7 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var StatsPlugin = require('stats-webpack-plugin');
 
 module.exports = {
-    // The entry file. All your app roots fromn here.
+    // The entry file. All your app roots from here.
     entry: [
         // Polyfills go here too, like babel-polyfill or whatwg-fetch
         'babel-polyfill',
